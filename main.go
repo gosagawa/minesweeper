f71ddcd2c6aa1634c07dc43636b7b947a9bd94f0
Go Sagawa <drunkturtle@gmail.com>
2020-01-15 00:51:46 +0900

    board

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("  X1X2X3X4X5X6X7X8X9")
+	fmt.Println("Y1・・・・・・・・・")
+	fmt.Println("Y2・・・・・・・・・")
+	fmt.Println("Y3・・・・・・・・・")
+	fmt.Println("Y4・・・・・・・・・")
+	fmt.Println("Y5・・・・・・・・・")
+	fmt.Println("Y6・・・・・・・・・")
+	fmt.Println("Y7・・・・・・・・・")
+	fmt.Println("Y8・・・・・・・・・")
+	fmt.Println("Y9・・・・・・・・・")
+}
